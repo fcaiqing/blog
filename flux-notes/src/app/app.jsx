194a238f046332b6abd543c67d84fd108b5df5d0
Cq <fcaiqing@163.com>
2019-06-05 09:49:29 +0800

    Create app.jsx

---
+++ flux-notes/src/app/app.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+import {NumberContainer} from '../components/Number/NumberContainer.jsx'
+
+export class App extends Component {
+    render() {
+        return (
+            <NumberContainer />
+        )
+    }
+}
